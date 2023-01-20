ed6bb5251fa08b4cd35c4e3e637fa91e19c85a8a
HK <sweetapplepie153@gmail.com>
2023-01-20 23:03:16 +0900

    01/20

---
+++ onecommit/src/onecommit/Solution4.java
@@ -0,0 +1,11 @@
+package onecommit;
+
+public class Solution4 {
+	    public int solution(int n, int t) {
+	        for (int i =0;i<t;i++){
+	            n=+n*2; 
+	      }
+	        return n;
+	    }
+	  
+}
