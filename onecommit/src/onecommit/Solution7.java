6d8eb390784280b6748efb8b4af265a9d9826bc0
HK <sweetapplepie153@gmail.com>
2023-01-23 22:28:41 +0900

    01/23

--- onecommit/src/onecommit/Solution7.java
+++ onecommit/src/onecommit/Solution7.java
@@ -24,6 +24,25 @@ public class Solution7 {
 	        
 	        return answer;
 	    }
+	
+	
+	
+//	class Solution {
+//	    public int solution(int n) {
+//	        int answer = 0;
+//
+//	        for (int i = 1; i <= n; i++) {
+//	            answer++;
+//	            if (answer % 3 == 0 || String.valueOf(answer).contains("3")) {
+//	                i--;
+//	            }
+//	        }
+//
+//	        return answer;
+//	    }
+//	}
+//	
+	
 	public static void main(String[] args) {
     	System.out.println(solution(15));
 	}
