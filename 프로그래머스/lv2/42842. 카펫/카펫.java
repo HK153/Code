a88bc746c39fc4ba58c2359ff155da9757a12698
HK153 <89124733+HK153@users.noreply.github.com>
2023-05-12 00:16:35 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -6,16 +6,18 @@ class Solution {
         ArrayList<Integer> a = new ArrayList<>();
         ArrayList<Integer> b = new ArrayList<>();
         int total = brown + yellow;
-        for (int i =1; i<total/2; i++){
+        for (int i =1; i<=Math.sqrt(total); i++){
             if (total%i == 0){
                 a.add(i);
                 b.add(total/i);
             }
         }
-        for (int j =0; j<a.size(); j++){
+
+        for (int j = a.size()-1; j>=0; j--){
             if (a.get(j)*b.get(j) == total && (a.get(j)-2)*(b.get(j)-2) == yellow){
-                answer[0]=a.get(j);
-                answer[1]=b.get(j);
+                answer[0]=b.get(j);
+                answer[1]=a.get(j);
+                break;
             }
         }
         
