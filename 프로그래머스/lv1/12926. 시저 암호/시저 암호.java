f0ebf31fdf9e2a612bff77479acf86c847e198fa
HK153 <89124733+HK153@users.noreply.github.com>
2023-05-20 16:57:38 +0900

    [level 1] Title: 시저 암호, Time: 35.86 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,19 +1,20 @@
 class Solution {
     public String solution(String s, int n) {
         String answer = "";
-        for (char c : s.toCharArray()){
+               
+        for (int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            
             if (c == ' '){
                 answer += ' ';
             }
-            else if (c >= 'A' && c <= 'Z' && (int)c + n > 'Z'){
-                answer += (char)(c + n - 26);    
+            else if (c >= 'A' && c <= 'Z'){
+                answer += (char)('A' + (c + n - 'A')%26);    
             }
-            else if (c >= 'a' && c<= 'z' && (int)c +n > 'z'){
-                answer += (char)(c + n - 26); 
+            else if (c >= 'a' && c<= 'z'){
+                answer += (char)('a' + (c + n - 'a')%26); 
             }
-            else {
-                answer += (char)(c + n); 
-            }
+
 
         }
         
