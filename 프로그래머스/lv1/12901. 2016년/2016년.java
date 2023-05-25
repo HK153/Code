73092e57ec2a42a038eda3f1be36361f66fde54b
HK153 <89124733+HK153@users.noreply.github.com>
2023-05-26 00:01:18 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -4,21 +4,12 @@ class Solution {
         String [] week = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
         int [] days = {31,29,31,30,31,30,31,31,30,31,30,31};
         int count = 0;
-        if (a==1){
-            count = b-1;
-            answer = week[count%7];
+        
+        for (int i =0; i<a-1; i++){
+            count += days[i];
         }
-        else if (a==2){
-            count = b+ days[0] -1;
-            answer = week[count%7];
-        }
-        else{
-            for (int i =0; i<a-1; i++){
-                count += days[i];
-            }
-            count+= b -1;
-            answer = week[count%7];
-        }
+        count+= b -1;
+        answer = week[count%7];
 
         return answer;
     }
