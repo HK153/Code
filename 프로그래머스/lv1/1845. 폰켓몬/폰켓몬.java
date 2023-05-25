369b1144e4c19bc988b1afb6579c79605139374a
HK153 <89124733+HK153@users.noreply.github.com>
2023-05-26 02:23:15 +0900

    [level 1] Title: 폰켓몬, Time: 1.73 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 1;
+        Arrays.sort(nums);
+        for (int i =1; i<nums.length; i++){
+            if (answer == nums.length/2){
+                break;
+            }
+            if(nums[i-1] != nums[i]){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
