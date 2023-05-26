40fc67a1721a138e2ba079fc13ac2f846abe7d26
HK153 <89124733+HK153@users.noreply.github.com>
2023-05-26 19:01:07 +0900

    [level 1] Title: 폰켓몬, Time: 3.45 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,16 +1,18 @@
-import java.util.Arrays;
+import java.util.HashSet;
 class Solution {
     public int solution(int[] nums) {
-        int answer = 1;
-        Arrays.sort(nums);
-        for (int i =1; i<nums.length; i++){
-            if (answer == nums.length/2){
-                break;
-            }
-            if(nums[i-1] != nums[i]){
-                answer++;
-            }
+        int answer = 0;
+        HashSet <Integer> numHash = new HashSet<>();
+        for (int n : nums){
+            numHash.add(n);
         }
+        if (numHash.size() >= nums.length/2){
+                answer= nums.length/2;
+        }
+        else {
+            answer = numHash.size();
+        }
+            
         return answer;
     }
 }
