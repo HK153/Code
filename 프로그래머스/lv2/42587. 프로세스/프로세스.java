9b1ce5277dc36c3cb374a2f5a11c0773f5a31bbe
HK153 <89124733+HK153@users.noreply.github.com>
2023-07-06 03:10:15 +0900

    [level 2] Title: 프로세스, Time: 0.65 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.java
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.java
@@ -1,49 +1,40 @@
-import java.util.Queue;
 import java.util.LinkedList;
-import java.util.Arrays;
-import java.util.ArrayList;
+import java.util.Queue;
 
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        ArrayList<Integer> order = new ArrayList<>();
-        Queue<String> qlist = new LinkedList<>();
+        Queue<Integer> queue = new LinkedList<>();
         
-        // 초기 순서 기록
-        for (int i = 0; i < priorities.length; i++) {
-            qlist.add(priorities[i] + "" + i);
+        for (int priority : priorities) {
+            queue.add(priority);
         }
         
-        // 중요도 max 값 구하기
-        Arrays.sort(priorities);
-        int maxIndex = priorities.length - 1;
-        int max = priorities[maxIndex];
+        int target = location;
         
-        // 우선순위에 따라 처리
-        while (!qlist.isEmpty()) {
-            if (Character.getNumericValue(qlist.peek().charAt(0)) == max) {
-                // 뽑은 프로세스의 순서 기록
-                order.add(Integer.parseInt(qlist.peek().substring(1)));
-                // 큐 삭제
-                qlist.remove();
-                // 우선순위 값 수정
-                maxIndex--;
-                if (maxIndex >= 0) {
-                    max = priorities[maxIndex];
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            boolean isMaxPriority = true;
+            
+            for (int priority : queue) {
+                if (priority > current) {
+                    isMaxPriority = false;
+                    break;
+                }
+            }
+            
+            if (isMaxPriority) {
+                answer++;
+                if (target == 0) {
+                    break;
                 }
             } else {
-                qlist.add(qlist.peek());
-                qlist.remove();
+                queue.add(current);
             }
+            
+            target = (target - 1 + queue.size()) % queue.size();
         }
         
-        // 프로세스 실행 순서 탐색
-        for (int j = 0; j < order.size(); j++) {
-            if (order.get(j) == location) {
-                answer = j + 1;
-            }
-        }
-       
         return answer;
     }
 }
