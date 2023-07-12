29f8c1b65cbf3510ab6caa38ea4a755502e60822
HK153 <89124733+HK153@users.noreply.github.com>
2023-07-12 22:28:44 +0900

    [level 1] Title: 소수 찾기, Time: 224.65 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -3,17 +3,14 @@ class Solution {
         int answer = 1;
         boolean flag = true;
         for (int i = 3; i<=n; i++){
-
             for (int j = 2; j<Math.sqrt(i)+1; j++){
                if (i%j == 0){
                    flag = false;
-                   
                    break;
                }            
             }
             if(flag) {
                 answer++;
-
                 }
             flag = true;
         }
