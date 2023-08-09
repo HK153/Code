943d0af191f0c827e3e74bb6ac67aeda57b32505
Hyunkyung <89124733+HK153@users.noreply.github.com>
2023-08-09 17:35:22 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.54 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -10,7 +10,7 @@ class Solution {
         }
         for (int j =0; j<s.length(); j++){
             for (Character data : link){
-                if (st.isEmpty() || st.peek() == ')'|| st.peek() == '}'|| st.peek() == ']' ){
+                if (st.isEmpty()){
                    st.push(data); 
                 }
                 else if (st.peek() == '(' && data == ')'){
