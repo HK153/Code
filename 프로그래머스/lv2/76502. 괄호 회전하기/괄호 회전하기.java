a858eac17ddd382ea921375244d005494bcd5ea0
Hyunkyung <89124733+HK153@users.noreply.github.com>
2023-08-09 16:27:07 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.49 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+import java.util.Stack;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        LinkedList <Character> link = new LinkedList<>();
+        Stack <Character> st = new Stack<>();
+        for (int i = 0; i<s.length(); i++){
+            link.add(s.charAt(i));
+        }
+        for (int j =0; j<s.length(); j++){
+            for (Character data : link){
+                if (st.isEmpty() || st.peek() == ')'|| st.peek() == '}'|| st.peek() == ']' ){
+                   st.push(data); 
+                }
+                else if (st.peek() == '(' && data == ')'){
+                    st.pop();
+                }
+                else if (st.peek() == '{' && data == '}'){
+                    st.pop();
+                }
+                else if (st.peek() == '[' && data == ']'){
+                    st.pop();
+                }
+                else {
+                    st.push(data);
+                }
+                
+            }
+            if (st.isEmpty()){
+                answer++;
+            }
+            st.clear();
+            Character a = link.getFirst();
+            link.removeFirst();
+            link.add(a);    
+        }
+        
+        return answer;
+    }
+}
