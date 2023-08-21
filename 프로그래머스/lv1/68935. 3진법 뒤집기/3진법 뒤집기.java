13e6ceb6bff1aa30e0fa2197f83c262dfeb2b923
Hyunkyung <89124733+HK153@users.noreply.github.com>
2023-08-21 17:27:19 +0900

    [level 1] Title: 3진법 뒤집기, Time: 6.74 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -2,17 +2,16 @@ import java.util.Stack;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        Stack <Character> st = new Stack<>();
+        Stack <String> st = new Stack<>();
         String a = Integer.toString(n, 3);
         for(int i =0; i<a.length(); i++){
-            st.push(a.charAt(i));
+            st.push(a.charAt(i)+"");
         }
         String num = "";
         for(int j =0; j<a.length(); j++){
             num += st.pop();
         }
-        long c = Long.parseLong(num);
-        String number = Long.toString(c);
-        return answer = Integer.parseInt(number,3);
+
+        return answer = Integer.parseInt(num,3);
     }
 }
