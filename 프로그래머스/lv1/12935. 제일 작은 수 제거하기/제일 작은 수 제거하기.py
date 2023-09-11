6dc686ad8db98257966c46d60b8c13825f166cff
Hyunkyung <89124733+HK153@users.noreply.github.com>
2023-09-11 16:08:02 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -1,11 +1,9 @@
 def solution(arr):
     answer = []
-    m = min(arr)
+
     if len(arr) == 1:
         answer.append(-1)
+        return answer
     else:
-        for a in arr:
-            if a != m:
-                answer.append(a)
-                
-    return answer
+        arr.remove(min(arr))
+        return arr        
