29c5e0a1d81bbd6e474325c053708e2acf8eeeaa
Hyunkyung <89124733+HK153@users.noreply.github.com>
2023-10-24 19:11:21 +0900

    [level 2] Title: 구명보트, Time: 13.42 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,6 +1,6 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=java) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 19:10:27
+2023년 10월 2일 19:11:18
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int sum = 0;
+
+        Arrays.sort(people);
+
+        Deque <Integer> deque = new ArrayDeque<>();
+        for (int p : people) {
+            deque.add(p);
+        }
+        
+        while (!deque.isEmpty()){
+            sum = deque.pollLast();
+            if (!deque.isEmpty() && sum + deque.peekFirst() <= limit){
+                deque.pollFirst();
+            }
+            answer += 1;
+        }
+ 
+        return answer;
+    }
+}
