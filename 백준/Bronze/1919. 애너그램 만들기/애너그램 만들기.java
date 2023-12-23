e595925652b3b9b80fd545e81bbb0f0240b9f96e
Hyunkyung <89124733+HK153@users.noreply.github.com>
2023-12-23 17:56:10 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 220 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {   
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String word1 = in.nextLine().trim();
+        String word2 = in.nextLine().trim();
+        
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+        
+       
+
+        for (char ch : word1.toCharArray()) {
+            frequencyMap.put(ch, frequencyMap.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : word2.toCharArray()) {
+            frequencyMap.put(ch, frequencyMap.getOrDefault(ch, 0) - 1);
+        }
+        
+         int answer = frequencyMap.values().stream()
+                .mapToInt(Math::abs)
+                .sum();
+        
+        System.out.println(answer);
+    }
+
+   
+}
