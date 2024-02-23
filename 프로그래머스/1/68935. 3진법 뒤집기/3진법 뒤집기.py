45ffce6cc8735bd28b824e00e71e2d7af3dad1e1
Hyunkyung <89124733+HK153@users.noreply.github.com>
2024-02-23 21:51:56 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,14 +1,10 @@
 def solution(n):
-    # 10진수를 3진수로 변환
-    a = ''
-    while n:
+    a_reversed = ''
+    while n > 0:
         n, mod = divmod(n, 3)
-        a = str(mod) + a
+        a_reversed += str(mod)
     
-    # 3진수를 뒤집음
-    a_reversed = a[::-1]
-
-    # 뒤집힌 3진수를 다시 10진수로 변환
+    
     answer = int(a_reversed, 3)
-    
+
     return answer
