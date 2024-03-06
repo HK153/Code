52dbdf76de6b91b20bf3bb6703a3e15c8b950a39
Hyunkyung <89124733+HK153@users.noreply.github.com>
2024-03-06 16:31:37 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT a.AUTHOR_ID, AUTHOR_NAME, CATEGORY, sum(SALES*PRICE) as TOTAL_SALES
+from BOOK_SALES s
+join BOOK b on s.BOOK_ID = b.BOOK_ID
+join AUTHOR a on b.AUTHOR_ID = a.AUTHOR_ID
+where SALES_DATE like '2022-01%'
+group by a.AUTHOR_ID, CATEGORY
+order by a.AUTHOR_ID ASC, CATEGORY DESC
