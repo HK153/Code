820f38d557afb9a76da1d85e686fdbc7fb0bce10
Hyunkyung <89124733+HK153@users.noreply.github.com>
2024-03-14 18:11:53 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,11 @@
+-- 코드를 작성해주세요
+select sum(g.SCORE) as SCORE, e.EMP_NO, e.EMP_NAME, e.POSITION, e.EMAIL
+from HR_GRADE g
+join HR_EMPLOYEES e
+on g.EMP_NO = e.EMP_NO
+group by e.EMP_NO, e.EMP_NAME, e.POSITION, e.EMAIL
+having e.EMP_NO = (select EMP_NO 
+                  from HR_GRADE 
+                  group by EMP_NO 
+                  order by sum(SCORE) DESC 
+                  limit 1)
