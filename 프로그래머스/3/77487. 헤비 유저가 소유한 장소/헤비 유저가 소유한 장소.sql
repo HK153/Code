af2cecf5d9f7e28592bd23c253fe2e22bdc4d56b
Hyunkyung <89124733+HK153@users.noreply.github.com>
2024-05-01 22:17:58 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SELECT 
+    ID, 
+    NAME, 
+    HOST_ID 
+from PLACES
+where HOST_ID IN (
+select HOST_ID
+    from PLACES 
+    group by HOST_ID 
+    having count(*) >= 2)
+ORDER by ID;
