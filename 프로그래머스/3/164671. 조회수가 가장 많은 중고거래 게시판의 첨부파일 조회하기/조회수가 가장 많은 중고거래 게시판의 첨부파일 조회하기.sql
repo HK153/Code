9d17074ad9570c4e23dc1ccf305c264321449c51
Hyunkyung <89124733+HK153@users.noreply.github.com>
2024-05-10 20:27:36 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -2,7 +2,9 @@
 SELECT 
     concat('/home/grep/src/', 
            f.BOARD_ID,'/', 
-           FILE_ID,FILE_NAME,FILE_EXT) as FILE_PATH
+           FILE_ID,
+           FILE_NAME,
+           FILE_EXT) as FILE_PATH
 from USED_GOODS_BOARD b
 join USED_GOODS_FILE f
 on b.BOARD_ID = f.BOARD_ID
