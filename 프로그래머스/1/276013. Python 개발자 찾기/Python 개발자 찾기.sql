e9cb4e6ff39fedebaeaa0e9bdae9bee8f0e10947
Hyunkyung <89124733+HK153@users.noreply.github.com>
2024-07-25 18:21:32 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,5 +1,7 @@
 -- 코드를 작성해주세요
-select ID, EMAIL, FIRST_NAME, LAST_NAME
+select 
+    ID, EMAIL, 
+    FIRST_NAME, LAST_NAME
 from DEVELOPER_INFOS
 where SKILL_1 = 'Python' 
     or SKILL_2 = 'Python' 
