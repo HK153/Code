7d627dcbc3e53443b3a0723e17f70e495be93f39
Hyunkyung <89124733+HK153@users.noreply.github.com>
2024-07-29 22:42:58 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,6 +1,5 @@
 -- 코드를 입력하세요
-SELECT 
-    ANIMAL_ID, 
-    NAME 
+SELECT ANIMAL_ID, NAME 
 from ANIMAL_INS
-where INTAKE_CONDITION Like "Sick";
+where INTAKE_CONDITION Like 'Sick'
+order by ANIMAL_ID
