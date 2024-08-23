93b4630d495cce266b8bc4e0b6abece9af5a5619
Hyunkyung <89124733+HK153@users.noreply.github.com>
2024-08-23 22:02:59 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/README.md
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/README.md
@@ -1,6 +1,6 @@
 # [level 2] 입양 시각 구하기(1) - 59412 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59412) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59412?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 03월 24일 22:18:48
+2024년 08월 23일 22:02:56
 
 ### 문제 설명
 
--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,6 +1,6 @@
 -- 코드를 입력하세요
-SELECT hour(DATETIME) as HOUR , count(*)
+SELECT TO_NUMBER(TO_CHAR(DATETIME, 'HH24')) as HOUR , count(*)
 from ANIMAL_OUTS
-where time(DATETIME) between '09:00:00' and '20:00:00'
-group by HOUR
-order by HOUR
+where TO_NUMBER(TO_CHAR(DATETIME, 'HH24')) between 9 and 20
+group by TO_NUMBER(TO_CHAR(DATETIME, 'HH24'))
+order by TO_NUMBER(TO_CHAR(DATETIME, 'HH24'))
