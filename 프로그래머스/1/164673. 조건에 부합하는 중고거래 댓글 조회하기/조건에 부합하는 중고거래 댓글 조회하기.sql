f448d9e247f0f89f65777b58de9d52b8ea872c9a
Hyunkyung <89124733+HK153@users.noreply.github.com>
2024-10-01 21:33:42 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,10 +1,7 @@
 -- 코드를 입력하세요
 SELECT 
-    b.TITLE, 
-    b.BOARD_ID, 
-    r.REPLY_ID, 
-    r.WRITER_ID, 
-    r.CONTENTS, 
+    b.TITLE, b.BOARD_ID, 
+    r.REPLY_ID, r.WRITER_ID, r.CONTENTS, 
     TO_CHAR(r.CREATED_DATE, 'YYYY-MM-DD') AS CREATED_DATE
 from USED_GOODS_BOARD b, USED_GOODS_REPLY r
 where b.BOARD_ID = r.BOARD_ID
