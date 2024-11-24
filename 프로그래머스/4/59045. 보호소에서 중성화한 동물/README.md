42f47291e8a37197a735af7d9bdd381f0e924a5f
Hyunkyung <89124733+HK153@users.noreply.github.com>
2024-11-24 21:09:00 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/README.md
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/README.md
@@ -1,6 +1,6 @@
 # [level 4] 보호소에서 중성화한 동물 - 59045 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59045?language=oracle) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59045) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 09월 23일 19:53:12
+2024년 11월 24일 21:08:59
 
 ### 문제 설명
 
--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -3,9 +3,12 @@ SELECT
     i.ANIMAL_ID, 
     i.ANIMAL_TYPE, 
     i.NAME
-from ANIMAL_INS i, ANIMAL_OUTS o
-where i.ANIMAL_ID = o.ANIMAL_ID
+from 
+    ANIMAL_INS i, 
+    ANIMAL_OUTS o
+where 
+    i.ANIMAL_ID = o.ANIMAL_ID
     and SEX_UPON_INTAKE like '%Intact%' 
     AND (SEX_UPON_OUTCOME like 'Neutered%'
-    or SEX_UPON_OUTCOME like 'Spayed%')
+        or SEX_UPON_OUTCOME like 'Spayed%')
 order by i.ANIMAL_ID
