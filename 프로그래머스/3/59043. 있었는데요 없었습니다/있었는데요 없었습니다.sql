6e279f96e91ff037a5e98a1629c1df23aa47ddf5
Hyunkyung <89124733+HK153@users.noreply.github.com>
2024-12-13 23:39:55 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -2,7 +2,11 @@
 SELECT 
     i.ANIMAL_ID, 
     i.NAME 
-from ANIMAL_INS i, ANIMAL_OUTS o 
-where i.ANIMAL_ID = o.ANIMAL_ID
+from 
+    ANIMAL_INS i, 
+    ANIMAL_OUTS o 
+where 
+    i.ANIMAL_ID = o.ANIMAL_ID
     and i.DATETIME > o.DATETIME 
-ORDER BY i.DATETIME;
+ORDER BY 
+    i.DATETIME;
