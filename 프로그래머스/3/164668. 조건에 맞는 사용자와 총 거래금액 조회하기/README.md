c875d745cdbb67b6644c184ef2f9b42d5e9828a0
Hyunkyung <89124733+HK153@users.noreply.github.com>
2025-03-06 22:49:28 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/README.md
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/README.md
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 09월 11일 23:05:53
+2025년 03월 06일 22:49:25
 
 ### 문제 설명
 
@@ -84,7 +84,7 @@ Empty
 <td>FALSE</td>
 </tr>
 <tr>
-<td>NICKANME</td>
+<td>NICKNAME</td>
 <td>VARCHAR(100)</td>
 <td>FALSE</td>
 </tr>
--- 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -1,9 +1,14 @@
 -- 코드를 입력하세요
-SELECT u.USER_ID, NICKNAME, 
+SELECT 
+    u.USER_ID, NICKNAME, 
     sum(PRICE) as TOTAL_SALES
-from USED_GOODS_BOARD b, USED_GOODS_USER u
-where b.WRITER_ID = u.USER_ID
-and STATUS = 'DONE'
-group by u.USER_ID, NICKNAME
-having sum(PRICE) >=700000
+from 
+    USED_GOODS_BOARD b, USED_GOODS_USER u
+where 
+    b.WRITER_ID = u.USER_ID
+    and STATUS = 'DONE'
+group by 
+    u.USER_ID, NICKNAME
+having 
+    sum(PRICE) >=700000
 order by 3
