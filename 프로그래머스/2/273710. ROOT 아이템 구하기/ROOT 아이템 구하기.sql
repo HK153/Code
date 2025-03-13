b4f645ef07dc81c014998244e9669e59da5fa4cf
Hyunkyung <89124733+HK153@users.noreply.github.com>
2025-03-13 22:45:01 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -2,8 +2,13 @@
 select 
     i.ITEM_ID, 
     ITEM_NAME
-from ITEM_INFO i 
-join ITEM_TREE t
-on i.ITEM_ID = t.ITEM_ID
-where PARENT_ITEM_ID is null 
-order by i.ITEM_ID ASC
+from 
+    ITEM_INFO i 
+join 
+    ITEM_TREE t
+on 
+    i.ITEM_ID = t.ITEM_ID
+where 
+    PARENT_ITEM_ID is null 
+order by 
+    i.ITEM_ID ASC
