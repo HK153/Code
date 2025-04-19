c2c3f64f46a77f8aa6850909e8b74fc48e32f5ce
Hyunkyung <89124733+HK153@users.noreply.github.com>
2025-04-19 22:56:22 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/README.md
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 저자 별 카테고리 별 매출액 집계하기 - 144856 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/144856?language=mysql) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/144856) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2025년 03월 05일 21:35:00
+2025년 04월 19일 22:56:19
 
 ### 문제 설명
 
--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,6 +1,9 @@
 -- 코드를 입력하세요
 SELECT 
-    a.AUTHOR_ID, AUTHOR_NAME, CATEGORY, sum(SALES*PRICE) as TOTAL_SALES
+    a.AUTHOR_ID, 
+    AUTHOR_NAME, 
+    CATEGORY, 
+    sum(SALES*PRICE) as TOTAL_SALES
 from 
     BOOK_SALES s
 join 
@@ -10,6 +13,8 @@ join
 where 
     SALES_DATE like '2022-01%'
 group by 
-    a.AUTHOR_ID, CATEGORY
+    a.AUTHOR_ID, 
+    CATEGORY
 order by 
-    a.AUTHOR_ID ASC, CATEGORY DESC
+    a.AUTHOR_ID ASC, 
+    CATEGORY DESC
