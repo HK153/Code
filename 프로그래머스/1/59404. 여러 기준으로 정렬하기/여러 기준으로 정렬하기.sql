1fab05c811f6332c1b18d4d508f0f6331f685e7d
Hyunkyung <89124733+HK153@users.noreply.github.com>
2025-07-31 22:49:21 +0900

    [level 1] Title: 여러 기준으로 정렬하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59404. 여러 기준으로 정렬하기/여러 기준으로 정렬하기.sql
+++ 프로그래머스/1/59404. 여러 기준으로 정렬하기/여러 기준으로 정렬하기.sql
@@ -1,4 +1,9 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME, DATETIME
+SELECT 
+    ANIMAL_ID, 
+    NAME, 
+    DATETIME
 from ANIMAL_INS
-order by NAME, DATETIME DESC;
+order by 
+    NAME ASC, 
+    DATETIME DESC
