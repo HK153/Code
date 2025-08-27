923d52949173899a83a7cb0fd5fa0b0c8cab6218
Hyunkyung <89124733+HK153@users.noreply.github.com>
2025-08-27 21:45:27 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.16 ms, Memory: 9.68 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
@@ -1,7 +1,2 @@
 def solution(n, k):
-    answer = []
-    for i in range(k, n+1,k):
-        if i%k == 0:
-            answer.append(i)
-        
-    return answer
+    return [i for i in range(k, n+1, k)]
